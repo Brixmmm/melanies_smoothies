f3b06aee5b57a6c36343936d030c7158b6cec0a0
Brixmmm <brice.saccucci@gmail.com>
2026-09-09 10:53:25 +0200

    Modify data selection and API request in streamlit_app
    Updated the data selection from the Snowflake table to include 'SEARCH_ON' column. Adjusted the API request to use 'search_on' instead of 'fruit_chosen'.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 
 
@@ -17,7 +18,8 @@ st.write('The name of your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas();
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -32,7 +34,8 @@ if ingredients_list:
 	for fruit_chosen in ingredients_list:
 		ingredients_string += fruit_chosen + ' '
 		st.subheader(fruit_chosen + ' Nutrition Information')
-		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+		
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
 		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 	
 	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
